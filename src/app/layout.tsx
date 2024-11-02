2a7befdf660f1d72a1e91e0740fd2f29816e8c20
kodenops <segun.femifadiya@gmail.com>
2024-11-02 14:18:13 +0100

    Formatted the broiler plate

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+	title: 'Manee',
+	description: 'Swift as a bullet, Reliable as an instinct',
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang='en'>
+			<body>{children}</body>
+		</html>
+	);
+}
