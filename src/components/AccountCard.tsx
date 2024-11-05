1a8ffcaab6009241167f914fa81e7e8b7a8c70a8
kodenops <segun.femifadiya@gmail.com>
2024-11-05 22:57:30 +0100

    updated the accountcard

--- src/components/AccountCard.tsx
+++ src/components/AccountCard.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FaRegCopy } from 'react-icons/fa6';
 import { IoEyeOff } from 'react-icons/io5';
-import design from '../../public/assets/accdesign.png';
+import design from '../../public/assets/bg.png';
 import Image from 'next/image';
 const AccountCard = () => {
 	return (
-		<div className='relative  h-[200px] rounded-md overflow-hidden flex-1 w-full'>
+		<div className='  h-[200px] rounded-md relative overflow-hidden flex-1 w-full'>
 			<div className='content  z-[100] flex flex-col h-full justify-between md:pl-[24px] pl-0 px-[8px] py-[20px]'>
-				<div className='top z-[20] '>
+				<div className='top z-[20] md:px-0 px-4 '>
 					<h4 className='md:text-md text-sm font-mono'>Savings</h4>
 					<p className='font-medium md:text-xl text-md tracking-widest flex items-center gap-2'>
 						0012202049{' '}
@@ -20,8 +20,8 @@ const AccountCard = () => {
 					<div className='font-medium text-md  md:w-[80%]'>
 						<p>Balance</p>
 					</div>
-					<div className='flex md:flex-row flex-col gap-2'>
-						<p className='md:text-[30px] text-[60px] tracking-widest font-bold text-left'>
+					<div className='flex md:flex-row flex-col gap-2 mr-4'>
+						<p className='md:text-[30px] text-[24px] tracking-widest font-bold text-left'>
 							$5,002,120.02
 						</p>
 						<p className='flex justify-center items-center md:flex-col gap-[5px]'>
@@ -31,12 +31,13 @@ const AccountCard = () => {
 					</div>
 				</div>
 			</div>
-			<div className='dark:bg-[#2a3f80f6] bg-[#aee9eb]  absolute h-full w-full top-0 z-[0] overflow-hidden'>
-				<div className='absolute bottom-0 md:w-[30vw]    right-0'>
+			<div className='dark:bg-[#2a3f80f6] bg-[#aee9eb]  absolute h-[100vh] z-[0] top-0	 overflow-hidden'>
+				<div className='top-0  h-full w-full object-cover bottom-0 scale-150 float-start  opacity-20 bg-red-300'>
 					<Image
 						src={design}
 						alt='flow'
-						height={600}
+						height={1000}
+						width={1000}
 					/>
 				</div>
 			</div>
