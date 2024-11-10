6f773d7dd9787cf44d7cc29b329bd16f73af78a4
kodenops <segun.femifadiya@gmail.com>
2024-11-10 17:25:37 +0100

    updated the size of switcher text

--- src/components/HeaderSwitcher.tsx
+++ src/components/HeaderSwitcher.tsx
@@ -20,14 +20,14 @@ const HeaderSwitcher = ({
 			{isFirstActive ? (
 				<div className='w-full shadow-md flex items-center justify-center md:h-[60px] h-[60px] rounded-sm'>
 					<button
-						className='leftMenu text-center bg-[var(--primary)] dark:bg-[var(--secondary-dark)] text-[var(--whites)] dark:text-[var(--primary-dark)] font-medium flex-1 h-full flex items-center justify-center'
+						className='leftMenu text-center bg-[var(--primary)] dark:bg-[var(--secondary-dark)] text-[var(--whites)] dark:text-[var(--primary-dark)] font-medium flex-1 h-full flex items-center md:text-lg text-[12px] justify-center'
 						onClick={() => {
 							setisFirstActive(true);
 						}}>
 						{buttonText1}
 					</button>
 					<button
-						className='rightMenu text-center bg-[var(--whites)] font-medium text-[var(--text)] dark:bg-[var(--card-bg-dark)] dark:text-[var(--whites-dark)] flex-1 h-full flex items-center justify-center'
+						className='rightMenu text-center bg-[var(--whites)] font-medium text-[var(--text)] dark:bg-[var(--card-bg-dark)] dark:text-[var(--whites-dark)] flex-1 h-full flex items-center justify-center md:text-lg text-[12px] '
 						onClick={() => {
 							setisFirstActive(false);
 						}}>
@@ -38,14 +38,14 @@ const HeaderSwitcher = ({
 				<>
 					<div className='w-full shadow-md flex items-center justify-center md:h-[60px] h-[60px]'>
 						<button
-							className='leftMenu text-center bg-[var(--whites)] font-medium text-[var(--text)] dark:bg-[var(--card-bg-dark)] dark:text-[var(--whites-dark)] flex-1 h-full flex items-center justify-center'
+							className='leftMenu text-center bg-[var(--whites)] font-medium text-[var(--text)] dark:bg-[var(--card-bg-dark)] dark:text-[var(--whites-dark)] flex-1 h-full flex items-center justify-center md:text-lg text-[12px] '
 							onClick={() => {
 								setisFirstActive(true);
 							}}>
 							{buttonText1}
 						</button>
 						<button
-							className='rightMenu text-center bg-[var(--primary)] dark:bg-[var(--secondary-dark)] text-[var(--whites)] dark:text-[var(--primary-dark)] font-medium flex-1 h-full flex items-center justify-center'
+							className='rightMenu text-center bg-[var(--primary)] dark:bg-[var(--secondary-dark)] text-[var(--whites)] dark:text-[var(--primary-dark)] font-medium flex-1 h-full flex items-center md:text-lg text-[12px]  justify-center'
 							onClick={() => {
 								setisFirstActive(false);
 							}}>
