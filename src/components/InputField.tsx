5115171fe674d4c9e73709465b130e67d1c78e27
kodenops <segun.femifadiya@gmail.com>
2024-11-13 07:30:53 +0100

    updated the input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -19,7 +19,7 @@ const InputField = ({
 				type={type}
 				placeholder={placeholder}
 				pattern={patterns}
-				className='w-full border-2 border-[var(--primary)] dark:border-[var(--inputborder)] rounded-[5px] px-[16px] md:py-[14px] py-[8px] placeholder:text-[var(--disabled)] focus:border-[var(--primary)] focus:outline-none dark:focus:border-[var(--inputborder)]'
+				className='w-full border-2 border-[var(--primary)] dark:border-[var(--inputborder)] rounded-[5px] px-[16px] md:py-[14px] py-[12px] placeholder:text-[var(--disabled)] focus:border-[var(--primary)] focus:outline-none dark:focus:border-[var(--inputborder)] bg-transparent'
 			/>
 			{withDesc === true ? <p>{infoText}</p> : null}
 		</div>
