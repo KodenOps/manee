953be9d17f2e7df1f381a4ccfad6a87b2f13a703
kodenops <segun.femifadiya@gmail.com>
2024-11-28 13:50:24 +0100

    updated the dark and light mode

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -11,7 +11,7 @@ export default function NotFound() {
 			<div className='md:block hidden'>
 				<SideNav />
 			</div>
-			<div className='bg-[var(--primary-dark)] h-full '>
+			<div className='dark:bg-[var(--primary-dark)] h-full '>
 				<HeaderNav />
 				<div className='md:pl-[210px] md:mt-[32px] mt-[48px] flex md:flex-row  flex-col items-center justify-center   w-full'>
 					<div className='md:w-[50%] w-[100%]'>
@@ -21,13 +21,15 @@ export default function NotFound() {
 						/>
 					</div>
 					<div className='md:w-[50%] px-[24px] flex flex-col gap-4'>
-						<h1 className='md:text-[50px] text-[32px]'>404 - Page Not Found</h1>
+						<h1 className='md:text-[50px] text-[32px] md:font-bold dark:text-[var(--whites)] text-[var(--primary-dark)]'>
+							404 - Page Not Found
+						</h1>
 						<p className='md:text-[24px] md:w-[80%] md:text-left text-center'>
 							The page you're looking for doesn't exist or has been moved.
 						</p>
 						<Link
 							href='/'
-							className='md:w-[20%] text-lg text-[var(--accents)] font-medium md:text-left text-center'>
+							className='md:w-[20%] text-lg dark:text-[var(--accents)] text-[var(--primary)] font-medium md:text-left text-center'>
 							Go Back Home
 						</Link>
 					</div>
