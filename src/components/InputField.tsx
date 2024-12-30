3ca19a5a1fe3f4ec63d8b5a77df357f7c69ec5e9
kodenops <segun.femifadiya@gmail.com>
2024-12-30 21:44:30 +0100

    UPDATE: Fix the deployment issue from last commit

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -7,7 +7,6 @@ interface inputType {
 	infoText?: string;
 	patterns?: string;
 	minval?: number;
-	Inputvalue: any;
 }
 const InputField = ({
 	placeholder,
@@ -16,7 +15,6 @@ const InputField = ({
 	infoText,
 	patterns,
 	minval,
-	Inputvalue,
 }: inputType) => {
 	return (
 		<div className='flex flex-col items-end'>
@@ -28,7 +26,6 @@ const InputField = ({
 				className='w-full border-2 border-[var(--primary)] dark:border-[var(--inputborder)] rounded-[5px] px-[16px] md:py-[14px] py-[12px] placeholder:text-[var(--disabled)] focus:border-[var(--primary)] focus:outline-none dark:focus:border-[var(--inputborder)] bg-transparent'
 				onChange={(e) => {
 					console.log(e.target.value);
-					Inputvalue = e.target.value;
 				}}
 			/>
 			{withDesc === true ? <p>{infoText}</p> : null}
